078087f14d4a6b18114fa622feb5c7c8133cb8c7
Brian <semperindoctrina19@gmail.com>
2021-07-01 18:03:00 -0500

    added dropdown

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 class Navbar {
     constructor(target, menu) {
         // 1. Check parameters type and throw error if not an HTML element
-        if (target instanceof HTMLElement && menu instanceof HTMLElement) {
-            // 2. If parameters are HTML elements set the attributes
-            this.btn = target;
-            this.menu = menu;
-            
-            target.addEventListener('click', () => {
-                this.open();
-            });
-        } else {
-            throw new TypeError("The Target and Menu arguments must be a DOM element."); 
+        if (!(target instanceof HTMLElement && menu instanceof HTMLElement)) {
+           throw new TypeError("The Target and Menu arguments must be a DOM element.");  
         }
+
         this.isOpen = false;
+        this.btn = target;
+        this.menu = menu;
+        this.btn.addEventListener('click', () => {
+            this.open();
+        });
     }
 
     open() {
@@ -24,6 +22,31 @@ class Navbar {
         this.isopen = !this.isopen;
     }
 }
+
+class Dropdown {
+    constructor(elem) {
+        if (!(elem instanceof HTMLElement)) {
+            throw new TypeError("The element must be an HTML element.");
+        }
+
+        this.isOpen = false;
+        this.elem = elem;
+        this.elem.addEventListener('click', () => {
+            this.open();
+        });
+    }
+    
+    open() {
+        if (!this.isOpen) {
+            this.elem.querySelector(".dropdown-list").classList.add("show-dropdown");
+        } else {
+            this.elem.querySelector(".dropdown-list").classList.remove("show-dropdown");
+        }
+        this.isOpen = !this.isOpen;
+    }
+}
+
+const dropdown = new Dropdown(document.querySelector(".dropdown"));
 
 const navbar = new Navbar(
     document.querySelector(".menu-btn"),
